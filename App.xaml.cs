f7b68aba550ee8377ff5a154ab3ad28083c552eb
João Pedro Araújo Gonçalves <araujoaopedro18@hotmail.com>
2026-05-24 22:22:52 -0300

    feat: App.xaml.cs - chamar LearnixDbInitializer.Seed no startup
    Add database initialization and seeding on startup.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,6 +1,5 @@
-﻿using System.Configuration;
-using System.Data;
 using System.Windows;
+using Learnix.data;
 
 namespace Learnix
 {
@@ -9,6 +8,24 @@ namespace Learnix
     /// </summary>
     public partial class App : Application
     {
-    }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
 
+            // Garante que o banco existe e popula dados iniciais (idempotente)
+            try
+            {
+                using (var ctx = new LearnixDbContext())
+                {
+                    LearnixDbInitializer.Seed(ctx);
+                }
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(
+                    "Erro ao inicializar o banco de dados:\n" + ex.Message,
+                    "Learnix", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
 }
